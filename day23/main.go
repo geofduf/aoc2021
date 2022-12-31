8197c7faa8350f45df9ecd2e5e83c0827a2a5d8c
Geof <46729592+geofduf@users.noreply.github.com>
2022-12-31 15:24:19 +0100

    refactoring

--- day23/main.go
+++ day23/main.go
@@ -10,11 +10,11 @@ import (
 
 // State representation. I should probably haved used a smaller structure for the hallway
 // but is was easier for me to figure out what to do using this one.
-// 		h: hallway
-// 		mobile: number of misplaced amphipods in each room
-// 		empty: number of empty spots in each room
-// 		cost: current cost
-// 		remaining: total number of misplaced amphipods (rooms & hallway)
+// h represents the hallway.
+// mobile represents the number of misplaced amphipods in each room.
+// empty represents the number of empty spots in each room.
+// cost represents the current cost.
+// remaining represents the total number of misplaced amphipods (rooms & hallway).
 type State struct {
 	h         [11]uint8
 	mobile    [4]uint8
@@ -53,31 +53,16 @@ type Key struct {
 
 const free = 4
 
-var (
-	experimental bool
-	spots        [7]uint8 = [7]uint8{0, 1, 3, 5, 7, 9, 10}
-	m            [][4]uint8
-	stack        []State
-	visited      map[Key]uint32
-	target       uint32      = 0
-	minCosts     [][5]uint32 = [][5]uint32{
-		{0, 2, 5, 10, 17},
-		{0, 20, 50, 110, 180},
-		{0, 200, 500, 1100, 1800},
-		{0, 2000, 5000, 10000, 17000},
-	}
-
-	// Probably faster than Math.Pow10() / switch / if-else statements
-	weights [4]uint32 = [4]uint32{1, 10, 100, 1000}
-)
-
 // Read + parse input from file, call processInput once or twice according to the input type
 // and print the answer(s).
 func main() {
 
 	start := time.Now()
 
-	var file string
+	var (
+		file         string
+		experimental bool
+	)
 
 	flag.StringVar(&file, "f", "input.txt", "Input file")
 	flag.BoolVar(&experimental, "e", false, "Enable experimental features")
@@ -89,14 +74,14 @@ func main() {
 	chars := re.ReplaceAll(input, []byte(""))
 
 	if len(chars) == 8 {
-		p1, t1 := processInput(chars)
+		p1, t1 := processInput(chars, experimental)
 		fmt.Printf("Part1: found cost of %d in %s\n", p1, t1)
 	} else {
 		var part1 []byte
 		part1 = append(part1, chars[:4]...)
-		p1, t1 := processInput(append(part1, chars[12:16]...))
+		p1, t1 := processInput(append(part1, chars[12:16]...), experimental)
 		fmt.Printf("Part1: found cost of %d in %s\n", p1, t1)
-		p2, t2 := processInput(chars)
+		p2, t2 := processInput(chars, experimental)
 		fmt.Printf("Part2: found cost of %d in %s\n", p2, t2)
 	}
 
@@ -105,17 +90,31 @@ func main() {
 }
 
 // Build the initial state, prepare the stack and start processing it.
-func processInput(chars []byte) (uint32, time.Duration) {
+func processInput(chars []byte, experimental bool) (uint32, time.Duration) {
 
 	start := time.Now()
 
-	m = [][4]uint8{}
+	// Some helpers.
+	var (
+		spots    [7]uint8    = [7]uint8{0, 1, 3, 5, 7, 9, 10}
+		minCosts [][5]uint32 = [][5]uint32{
+			{0, 2, 5, 10, 17},
+			{0, 20, 50, 110, 180},
+			{0, 200, 500, 1100, 1800},
+			{0, 2000, 5000, 10000, 17000},
+		}
+
+		// Probably faster than Math.Pow10() / switch / if-else statements.
+		weights [4]uint32 = [4]uint32{1, 10, 100, 1000}
+	)
+
+	rooms := [][4]uint8{}
 
 	for i, v := range chars {
 		if i%4 == 0 {
-			m = append(m, [4]uint8{})
+			rooms = append(rooms, [4]uint8{})
 		}
-		m[len(m)-1][i%4] = v - 65
+		rooms[len(rooms)-1][i%4] = v - 65
 	}
 
 	s := State{
@@ -123,8 +122,8 @@ func processInput(chars []byte) (uint32, time.Duration) {
 		remaining: uint8(len(chars)),
 	}
 
-	for r := len(m) - 1; r >= 0; r-- {
-		for c, v := range m[r] {
+	for r := len(rooms) - 1; r >= 0; r-- {
+		for c, v := range rooms[r] {
 			if int(v) == c && s.mobile[c] == 0 {
 				s.remaining -= 1
 			} else {
@@ -133,123 +132,122 @@ func processInput(chars []byte) (uint32, time.Duration) {
 		}
 	}
 
-	target = 0
-	stack = []State{s}
-	visited = make(map[Key]uint32)
+	target := uint32(0)
+	stack := []State{s}
+	visited := make(map[Key]uint32)
 
+STACK:
 	for len(stack) > 0 {
+
 		n := len(stack) - 1
 		s = stack[n]
 		stack = stack[:n]
-		run(&s)
-	}
 
-	return target, time.Since(start)
+		// Process state. The general pattern is to perform all possible hallway->room and room->room
+		// moves in-place (swapping not implemented) then start stacking new states for room-hallway
+		// moves.
 
-}
+		done := false
 
-// Process state. The general pattern is to perform all possible hall->room and room->room
-// moves in-place (swapping not implemented) then start stacking new states for room-hallway
-// moves.
-func run(s *State) {
+		// Restart the search until we can't perform any in-place moves (a move might enable other ones).
+		for !done {
 
-	done := false
+			done = true
 
-	// Restart the search until we can't perform any moves (a move might enable other ones).
-	for !done {
-
-		done = true
-
-		// In-place hallway->room
-		for _, src := range spots {
-			v := s.h[src]
-			if v != free {
-				dst := (v + 1) * 2
-				if s.mobile[v] == 0 && s.pathClear(src, dst) {
-					s.cost += uint32((absDiff(dst, src) + s.empty[v])) * weights[v]
-					if s.cost >= target && target != 0 {
-						return
+			// Hallway->room.
+			for _, src := range spots {
+				v := s.h[src]
+				if v != free {
+					dst := (v + 1) * 2
+					if s.mobile[v] == 0 && s.pathClear(src, dst) {
+						s.cost += uint32((absDiff(dst, src) + s.empty[v])) * weights[v]
+						if s.cost >= target && target != 0 {
+							continue STACK
+						}
+						if s.remaining == 1 {
+							target = s.cost
+							continue STACK
+						}
+						s.h[src] = free
+						s.empty[v]--
+						s.remaining--
+						done = false
 					}
-					if s.remaining == 1 {
-						target = s.cost
-						return
-					}
-					s.h[src] = free
-					s.empty[v]--
-					s.remaining--
-					done = false
 				}
 			}
+
+			// Room->room.
+			for c := uint8(0); c < 4; c++ {
+				if s.mobile[c] > 0 {
+					r := s.empty[c]
+					v := rooms[r][c]
+					src := (c + 1) * 2
+					dst := (v + 1) * 2
+					if s.mobile[v] == 0 && s.pathClear(src, dst) {
+						s.cost += uint32((absDiff(dst, src))+r+1+s.empty[v]) * weights[v]
+						if s.cost >= target && target != 0 {
+							continue STACK
+						}
+						s.empty[c]++
+						s.mobile[c]--
+						s.empty[v]--
+						s.remaining--
+						done = false
+					}
+				}
+			}
+
 		}
 
-		// In-place room->room
+		// Enable with -e flag. Trying to discard a few states without adding complexity.
+		// Evaluating a very approximate minimum remaining cost using each room theorical "best case"
+		// remaining cost. The value is added to the cost when challenging the current target
+		// in the next block.
+		var min uint32
+		if experimental && target != 0 {
+			min = minCosts[0][s.mobile[0]+s.empty[0]] +
+				minCosts[1][s.mobile[1]+s.empty[1]] +
+				minCosts[2][s.mobile[2]+s.empty[2]] +
+				minCosts[3][s.mobile[3]+s.empty[3]]
+		}
+
+		// New states (room->hallway).
 		for c := uint8(0); c < 4; c++ {
 			if s.mobile[c] > 0 {
 				r := s.empty[c]
-				v := m[r][c]
 				src := (c + 1) * 2
-				dst := (v + 1) * 2
-				if s.mobile[v] == 0 && s.pathClear(src, dst) {
-					s.cost += uint32((absDiff(dst, src))+r+1+s.empty[v]) * weights[v]
-					if s.cost >= target && target != 0 {
-						return
-					}
-					s.empty[c]++
-					s.mobile[c]--
-					s.empty[v]--
-					s.remaining--
-					done = false
-				}
-			}
-		}
-
-	}
-
-	// Enable with -e flag. Trying to discard a few states without adding complexity.
-	// Evaluating a very approximate minimum remaining cost using each room theorical "best case"
-	// remaining cost. The value is added to the cost when challenging the current target
-	// in the next block.
-	var min uint32
-	if experimental && target != 0 {
-		min = minCosts[0][s.mobile[0]+s.empty[0]] +
-			minCosts[1][s.mobile[1]+s.empty[1]] +
-			minCosts[2][s.mobile[2]+s.empty[2]] +
-			minCosts[3][s.mobile[3]+s.empty[3]]
-	}
-
-	// New states (room->hallway)
-	for c := uint8(0); c < 4; c++ {
-		if s.mobile[c] > 0 {
-			r := s.empty[c]
-			src := (c + 1) * 2
-			for _, dst := range spots {
-				if s.h[dst] == free && s.pathClear(src, dst) {
-					v := m[r][c]
-					cost := s.cost + (uint32(absDiff(dst, src)+r+1) * weights[v])
-					// Enable with -e flag. Trying to discard a few states without adding
-					// complexity. As we take the (best-case) cost to fill the
-					// room into account, we evaluate how much it would cost to move the amphipod
-					// next to the room (closest hallway spot).
-					var forecast uint32
-					if experimental {
-						forecast = (uint32(absDiff(dst, (v+1)*2)) - 1) * weights[v]
-					}
-					if cost+min+forecast < target || target == 0 {
-						next := *s
-						next.h[dst] = v
-						next.empty[c]++
-						next.mobile[c]--
-						next.cost = cost
-						key := Key{h: next.h, empty: next.empty, mobile: next.mobile}
-						if value, ok := visited[key]; !ok || cost < value {
-							stack = append(stack, next)
-							visited[key] = cost
+				for _, dst := range spots {
+					if s.h[dst] == free && s.pathClear(src, dst) {
+						v := rooms[r][c]
+						cost := s.cost + (uint32(absDiff(dst, src)+r+1) * weights[v])
+						// Enable with -e flag. Trying to discard a few states without adding
+						// complexity. As we take the (best-case) cost to fill the
+						// room into account, we evaluate how much it would cost to move the amphipod
+						// next to the room (closest hallway spot).
+						var forecast uint32
+						if experimental {
+							forecast = (uint32(absDiff(dst, (v+1)*2)) - 1) * weights[v]
+						}
+						if cost+min+forecast < target || target == 0 {
+							next := s
+							next.h[dst] = v
+							next.empty[c]++
+							next.mobile[c]--
+							next.cost = cost
+							key := Key{h: next.h, empty: next.empty, mobile: next.mobile}
+							if value, ok := visited[key]; !ok || cost < value {
+								stack = append(stack, next)
+								visited[key] = cost
+							}
 						}
 					}
 				}
 			}
 		}
+
 	}
+
+	return target, time.Since(start)
 
 }
 
